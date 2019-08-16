55c5824fa7d53434672f291ec2afd70bf0a1f80b
Dan <Danny13653@gmail.com>
2019-08-16 15:42:20 +0100

    *Imported WindowCapture & Global Variables

---
+++ TapTapFarmer/GlobalVariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapTapFarmer
+{
+    class GlobalVariables
+    {
+        //Add Globals here
+        public static bool BOT_STARTED = false;
+        public static String BOT_STATE = "Stopped";
+        public static String GLOBAL_PROC_NAME = "Nox"; //Nox is Default Application
+    }
+}
